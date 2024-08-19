25f3bc05ffb909e013656c6694f81a90807bb3d4
hyanghyanging <ghddbgid2596@gmail.com>
2024-08-20 00:20:53 +0900

    [Silver IV] Title: 복권, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/1359. 복권/복권.py
@@ -0,0 +1,24 @@
+import sys
+from itertools import combinations
+
+n, m, k = map(int, sys.stdin.readline().split())  
+'''
+1부터 N까지 수 중 서로 다른 M개의 수. 
+정답과 적어도 K개의 수가 같으면 당첨
+'''
+lottery = [i for i in range(1, n+1)]     # 가능한 숫자 리스트
+pick_comb = [*combinations(lottery, m)]  # 모든 숫자 조합
+
+winning = 0
+
+# 당첨 조합 개수 세기
+for pick in pick_comb:
+    for win_comb in combinations(lottery,m):
+        cnt = len(set(pick) & set(win_comb))   # 맟춘 숫자의 개수
+        if cnt >= k:
+            winning += 1
+
+# 전체 조합 개수
+total = len(pick_comb)**2
+
+print(winning/total)
