00cf6ae39665828eb7608ccea83b1f23fe3a0bce
hyanghyanging <ghddbgid2596@gmail.com>
2024-08-24 00:15:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline())
+bag = 0
+
+while n >= 0:
+    if n % 5 == 0:
+        bag += n//5
+        print(bag)
+        break
+    
+    n -= 3
+    bag +=1
+
+else:
+    bag = -1
+    print(bag)
