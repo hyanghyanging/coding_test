4f4fe0fe276ac34b5a9c10912b1ea0e4d07e6904
hyanghyanging <ghddbgid2596@gmail.com>
2024-09-08 00:45:19 +0900

    [Silver III] Title: 반전 요세푸스, Time: 60 ms, Memory: 34036 KB -BaekjoonHub

---
+++ 백준/Silver/20301. 반전 요세푸스/반전 요세푸스.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+N, K, M = map(int, sys.stdin.readline().split())
+array = deque([i for i in range(1, N+1)])
+result = []
+direction = 1   # 시계방향 : 1, 반시계방향 : -1
+remove_num = 0    # 제거된 횟수 기록
+
+while array:
+    if direction == 1:  # 시계방향
+        array.rotate(-(K-1))   # 반시계방향으로 (k-1)칸 이동
+    else:
+        array.rotate(K)        # 시계방향으로 K칸 이동
+
+    result.append(array.popleft())
+    remove_num += 1
+
+    # M번째 제거 마다 방향 전환
+    if remove_num % M == 0:
+        direction *= -1
+
+for num in result:
+    print(num)
