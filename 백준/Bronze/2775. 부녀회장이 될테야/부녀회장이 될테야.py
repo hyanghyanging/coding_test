08b106d7bd918fd85edf0b459019051729875e13
hyanghyanging <ghddbgid2596@gmail.com>
2024-11-01 23:53:22 +0900

    [Bronze I] Title: 부녀회장이 될테야, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2775. 부녀회장이 될테야/부녀회장이 될테야.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(sys.stdin.readline())
+for _ in range(t):
+    k = int(sys.stdin.readline())
+    n = int(sys.stdin.readline())
+
+    people = [x for x in range(1, n+1)]  
+    for _ in range(k):
+        for i in range(1, n): 
+            people[i] += people[i-1] 
+    print(people[n-1])
